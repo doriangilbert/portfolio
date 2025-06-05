4ffba0e2969a7c4221afcdf4a75ffb04839218f6
Dorian GILBERT <29594904+doriangilbert@users.noreply.github.com>
2025-06-05 10:31:22 +0200

    fix: Delete base path for domain setup

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    base: '/portfolio/',
-    plugins: [
-        vue(),
-        tailwindcss()
-    ],
-})
+  base: "/",
+  plugins: [vue(), tailwindcss()],
+});
